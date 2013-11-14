b7dc8883dd21ca0aac74a74c5c6701d1506a470d
kadmin <kadmin@172.16.220.148>
2013-11-14 11:07:38 -0600

    added a moon boolean

--- RelationshipsPJE/src/relationshipspje/RelationshipsPJE.java
+++ RelationshipsPJE/src/relationshipspje/RelationshipsPJE.java
@@ -15,6 +15,6 @@ public class RelationshipsPJE {
      */
     public static void main(String[] args) {
         int numPototoes;
-        boolean thatsAMoon;
+        boolean thatsAMoon = true;
     }
 }
