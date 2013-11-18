03b71b570be18e48514759afe2612458c1b8631a
john.woodhouse <john.woodhouse@172.16.219.123>
2013-11-18 11:23:39 -0600

    worked on PeopleClass's return values

--- RelationshipsPJE/src/relationshipspje/PeopleClass.java
+++ RelationshipsPJE/src/relationshipspje/PeopleClass.java
@@ -10,5 +10,25 @@ package relationshipspje;
  */
 public class PeopleClass 
 {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private boolean isMale;
     
+    public int getId()
+    {
+        return id;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public boolean getIsMale()
+    {
+        return isMale;
+    }
 }
