96c034d87e002f7670f749634720bdf42ee5e12f
john.woodhouse <john.woodhouse@172.16.219.123>
2013-11-19 11:19:49 -0600

    added params and comments for PeopleClass(newId, newFirstName, newLastName, newIsMale)

--- RelationshipsPJE/src/relationshipspje/PeopleClass.java
+++ RelationshipsPJE/src/relationshipspje/PeopleClass.java
@@ -26,6 +26,13 @@ public class PeopleClass
         
         relations=new TreeMap<String,ArrayList<Integer>>();
     }
+    /**precondition: need to receive information from Controller 
+     * 
+     * @param newId The new id number for the new PeopleClass person
+     * @param newFirstName The first name of the new person
+     * @param newLastName The last name of the new person
+     * @param newIsMale Whether or not the new person is male
+     */
     public PeopleClass(int newId, String newFirstName, String newLastName, boolean newIsMale)
     {
         id = newId;
