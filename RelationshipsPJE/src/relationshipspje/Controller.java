5329f0891ed12e69a3ade9f37556883ff795619b
phillip.johnson <phillip.johnson@172.16.217.142>
2013-11-19 11:35:19 -0600

    Added logic for reading the relationship type file. There is a broken constructor, but it should work. We shall see!
    Controller to do:
    Relations reader logic
    Save function

--- RelationshipsPJE/src/relationshipspje/Controller.java
+++ RelationshipsPJE/src/relationshipspje/Controller.java
@@ -232,7 +232,7 @@ public class Controller
                     
                     
                     //Make a new person, and put it into my list of people, with the id as the dictionary key.
-                    RelationType theNewRel = new RelationType(id, male, female, maleInv, femaleInv);
+                    RelationType theNewRel = new RelationType(id, neutral, male, female, maleInv, femaleInv, neutral);
                     
                     relationList.put(id,theNewRel);
                     
