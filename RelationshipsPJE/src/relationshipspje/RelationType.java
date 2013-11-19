abfa34349ed2114c264965a61b9ae8dbed1a61d7
john.woodhouse <john.woodhouse@172.16.219.123>
2013-11-19 11:38:03 -0600

    precondition added to RelationType

--- RelationshipsPJE/src/relationshipspje/RelationType.java
+++ RelationshipsPJE/src/relationshipspje/RelationType.java
@@ -24,8 +24,22 @@ public class RelationType
     {
         
     }
+    /** Precondition: data must be given from Controller
+     * 
+     * @param newId
+     * @param newType
+     * @param newMaleName
+     * @param newFemaleName
+     * @param newInverseMaleName
+     * @param newInverseFemaleName 
+     */
     public RelationType(int newId, String newType, String newMaleName, String newFemaleName, String newInverseMaleName, String newInverseFemaleName)
     {
-        
+        id=newId;
+        type=newType;
+        maleName=newMaleName;
+        femaleName=newFemaleName;
+        inverseMaleName=newInverseMaleName;
+        inverseFemaleName=newInverseFemaleName;
     }
 }
